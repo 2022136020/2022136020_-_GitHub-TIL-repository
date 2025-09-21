9d81356a045d199d3a5bc326c609aa7dbd77e50c
2022136020 <minam3200@g.shingu.ac.kr>
2025-09-21 19:34:14 +0900

    Week_03
    3주차 공부 내용

---
+++ Week_03.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  // var author = '개발하는           남자';
+  // String? name = "주성현";
+  // print(author);
+  // print(name);
+
+  // var fixedList = List<int>.filled(3, 0); // 3은 개수 0은 입력할 숫자
+  // print(fixedList);
+
+
+  // var fruits = <String>{'사과', '바나나', '사과'};
+  // print(fruits); 
+
+ 
+ }
